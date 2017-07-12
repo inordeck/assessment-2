8af7e2c5fa8db4326932f391642e4ab53308db3d
inordeck <ian.nordeck@gmail.com>
2017-07-12 08:28:04 -0600

    working on game logic

--- js/app.js
+++ js/app.js
@@ -1,14 +1,133 @@
-console.log("we're working over here");
+console.log("we're (not) working over here (yet");
 
-/* MONSTER 1 = A=65 / S=83 */
-/* MONSTER 2 = K=75 / L=76 */
+window.onload = function(){
+	monsters = {
+		monsterOne: {
+			div: document.getElementById("monsterOne"),
+			name: "Monster One"
+		},
+		monsterTwo: {
+			div: document.getElementById("monsterTwo"),
+			name: "Monster Two"
+		}
+	};
+};
+
+var monsters = {};
+
+function startGame(monsters){
+	monsters.monsterOne.div.style.left = 0;
+	monsters.monsterTwo.div.style.left = 0;
+}
+
+document.getElementsByTagName("img").className = "";
+
+document.onkeydown = function(key) {
+	monsters.monsterOne.position = parseInt(monsters.monsterOne.div.style.left, 10);
+	monsters.monsterTwo.position = parseInt(monsters.monsterTwo.div.style.left, 10);
+	checkWinner();
+	movePlayer(key);
+};
+
+function checkWinner() {
+	if (monsters.monsterOne.position + monsters.monsterOne.div.offsetWidth >= window.innerWidth - 40) {
+		setWinState(monsters.monsterOne, monsters);
+	}
+	if (monsters.monsterTwo.position + monsters.monsterTwo.div.offsetWidth >= window.innerWidth - 40) {
+		setWinState(monsters.monsterTwo, monsters);
+	}
+}
+
+function movePlayer(key) {
+	switch(key.which) {
+		case 65:  // press a to move monster one
+			var newMonsterOnePosition = monsters.monsterOne.position += 40;
+			monsters.monsterOne.div.style.left = newMonsterOnePosition + "px";
+			break;
+		case 75:  // press k to move monster one
+			var newMonsterTwoPosition = monsters.monsterTwo.position += 40;
+			monsters.monsterTwo.div.style.left = newMonsterTwoPosition + "px";
+			break;
+	}
+}
+
+function setWinState(player, monsters) {
+	document.onkeydown = null;
+	var winner = document.createElement("h1");
+	document.getElementsByTagName("main").appendChild(winner);
+	document.getElementsByTagName("h1") [0].innerText = monsters.name + " Wins!";
+}
 
 
-setInterval(moveMonsterOne, 20);
+/* MONSTER 1 = A=65 / S=83 
+	class: monsters id: monsterOne */
+/* MONSTER 2 = K=75 / L=76 
+	class: monsters id: monsterTwo */
+
+
+
+/*
+var keys = {};
+	keys.UP = 38;
+	keys.LEFT = 37;
+	keys.RIGHT = 39;
+	keys.DOWN = 40;
+console.log(keys);
+
+var mOne = {
+	x: 100, //horizontal
+	y: 100, //vertical
+	speedMultiplier: 5,
+	element: document.getElementById("monsterOne")
+};
+console.log(mOne);
+
+document.body.onkeyup =
+document.body.onkeydown = function(e){
+	if (e.preventDefault) {
+		e.preventDefault();
+	} else {
+		e.returnValue = false;
+	}
+	var kc = e.keyCode || e.which;
+	keys[kc] = e.type == 'keydown';
+};
+
+var moveMonsterOne = function(dx, dy){
+	mOne.x += (dx||0) * mOne.speedMultiplier;
+	mOne.y += (dy||0) * mOne.speedMultiplier;
+	mOne.element.style.left = mOne.x + 'px';
+	mOne.element.style.top = mOne.y + 'px';
+};
+
+var detectMovementOne = function() {
+	if ( keys[keys.LEFT] ) {
+		moveMonsterOne(-1, 0);
+	}
+	if ( keys[keys.RIGHT] ) {
+		moveMonsterOne(1, 0);
+	}
+	if ( keys[keys.UP] ) {
+		moveMonsterOne(0, -1);
+	}
+	if ( keys[keys.DOWN] ) {
+		moveMonsterOne(0, 1);
+	}
+};
+moveMonsterOne();
+
+setInterval(function(){
+	detectMovementOne();
+}, 1000/24);
+*/
+
+
+
+/*setInterval(moveMonsterOne, 20);
 var keys = {};
 
-//setInterval(moveMonsterTwo, 20);
-//var keys ={};
+setInterval(moveMonsterTwo, 20);
+var keys ={};
 
 $(document).keydown(function(e) {
 	keys[e.keyCode] = true;
@@ -18,7 +137,6 @@ $(document).keyup(function(e){
 	delete keys[e.keyCode];
 });
 
-/* move player 1 */
 function moveMonsterOne() {
 	for (var direction in keys) {
 		if (!keys.hasOwnProperty(direction)) continue;
@@ -27,6 +145,7 @@ function moveMonsterOne() {
 		}
 	}
 }
+moveMonsterOne();*/
 
 
 
